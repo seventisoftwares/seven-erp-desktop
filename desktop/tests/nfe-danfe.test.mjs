429fe93eb3d47365feaaf0ebb85a49ea0b9b50c5
seventisoftwares <suporte01@seventitecnologia.com.br>
2026-09-02 07:38:48 -0300

    NF-e: valida estrutura clássica do DANFE

--- desktop/tests/nfe-danfe.test.mjs
+++ desktop/tests/nfe-danfe.test.mjs
@@ -1,16 +1,17 @@
 import test from "node:test";
 import assert from "node:assert/strict";
-import { buildDanfeHtml, code128Svg, encodeCode128, parseNfeProc } from "../src/nfe-danfe.mjs";
+import { buildDanfeHtml, code128Svg, encodeCode128, extractClassicData, parseNfeProc } from "../src/nfe-danfe.mjs";
 
 const ACCESS_KEY = "43260912345678000195550010000001231000001234";
 const NFE_PROC = `<?xml version="1.0" encoding="UTF-8"?>
 <nfeProc xmlns="http://www.portalfiscal.inf.br/nfe" versao="4.00">
   <NFe><infNFe Id="NFe${ACCESS_KEY}" versao="4.00">
-    <ide><cUF>43</cUF><natOp>VENDA DE MERCADORIA</natOp><mod>55</mod><serie>1</serie><nNF>123</nNF><dhEmi>2026-09-01T10:00:00-03:00</dhEmi><tpAmb>2</tpAmb></ide>
+    <ide><cUF>43</cUF><natOp>VENDA DE MERCADORIA</natOp><mod>55</mod><serie>1</serie><nNF>123</nNF><dhEmi>2026-09-01T10:00:00-03:00</dhEmi><tpNF>1</tpNF><tpAmb>2</tpAmb></ide>
     <emit><CNPJ>12345678000195</CNPJ><xNome>SEVEN EMPRESA TESTE LTDA</xNome><xFant>SEVEN TESTE</xFant><enderEmit><xLgr>Rua Emitente</xLgr><nro>100</nro><xBairro>Centro</xBairro><xMun>Porto Alegre</xMun><UF>RS</UF><CEP>90000000</CEP><fone>51999999999</fone></enderEmit><IE>1234567890</IE></emit>
     <dest><CPF>52998224725</CPF><xNome>CLIENTE TESTE &amp; CIA</xNome><enderDest><xLgr>Rua Cliente</xLgr><nro>200</nro><xBairro>Centro</xBairro><xMun>Porto Alegre</xMun><UF>RS</UF><CEP>90000001</CEP></enderDest><indIEDest>9</indIEDest></dest>
     <det nItem="1"><prod><cProd>P001</cProd><xProd>Produto de Teste</xProd><NCM>84713012</NCM><CFOP>5102</CFOP><uCom>UN</uCom><qCom>2.0000</qCom><vUnCom>50.0000000000</vUnCom><vProd>100.00</vProd></prod><imposto><ICMS><ICMSSN102><orig>0</orig><CSOSN>102</CSOSN></ICMSSN102></ICMS></imposto></det>
     <total><ICMSTot><vBC>0.00</vBC><vICMS>0.00</vICMS><vProd>100.00</vProd><vFrete>0.00</vFrete><vSeg>0.00</vSeg><vDesc>0.00</vDesc><vIPI>0.00</vIPI><vPIS>0.00</vPIS><vCOFINS>0.00</vCOFINS><vOutro>0.00</vOutro><vNF>100.00</vNF></ICMSTot></total>
+    <transp><modFrete>9</modFrete></transp>
     <infAdic><infCpl>Pedido interno 123</infCpl></infAdic>
   </infNFe></NFe>
   <protNFe versao="4.00"><infProt><tpAmb>2</tpAmb><chNFe>${ACCESS_KEY}</chNFe><dhRecbto>2026-09-01T10:00:05-03:00</dhRecbto><nProt>143260000000001</nProt><cStat>100</cStat><xMotivo>Autorizado o uso da NF-e</xMotivo></infProt></protNFe>
@@ -48,12 +49,25 @@ test("Code 128 encoder supports numeric/alphanumeric NF-e keys and produces SVG 
   assert.match(svg, /<rect /);
 });
 
-test("DANFE HTML marks homologation and cancelled documents explicitly", () => {
-  const homologation = buildDanfeHtml({ nfeProcXml: NFE_PROC });
-  assert.match(homologation, /SEM VALOR FISCAL · HOMOLOGAÇÃO/);
-  assert.match(homologation, /DANFE/);
-  assert.match(homologation, /143260000000001/);
-  assert.match(homologation, /CLIENTE TESTE &amp; CIA/);
+test("DANFE clássico traz blocos tradicionais do modelo 55", () => {
+  const data = extractClassicData(NFE_PROC);
+  assert.equal(data.operationType, "1 - SAÍDA");
+  assert.equal(data.transport.mode, "9");
+  const html = buildDanfeHtml({ nfeProcXml: NFE_PROC });
+  assert.match(html, /RECEBEMOS DE/);
+  assert.match(html, /DANFE/);
+  assert.match(html, /Chave de acesso/);
+  assert.match(html, /Destinatário \/ Remetente/);
+  assert.match(html, /Cálculo do imposto/);
+  assert.match(html, /Transportador \/ Volumes transportados/);
+  assert.match(html, /Dados dos produtos \/ serviços/);
+  assert.match(html, /SEM VALOR FISCAL/);
+  assert.match(html, /AMBIENTE DE HOMOLOGAÇÃO/);
+  assert.match(html, /143260000000001/);
+  assert.match(html, /CLIENTE TESTE &amp; CIA/);
+});
+
+test("DANFE clássico marca cancelamento e protocolo do evento", () => {
   const cancelled = buildDanfeHtml({ nfeProcXml: NFE_PROC, cancelled: true, cancellationProtocol: "143260000000099" });
   assert.match(cancelled, /CANCELADA/);
   assert.match(cancelled, /Protocolo do evento: 143260000000099/);
